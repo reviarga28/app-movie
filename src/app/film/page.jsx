f07aad6aec0e2b7be56fe88123dee53e338cb4de
revi <reviarga07@gmail.com>
2025-02-17 08:37:21 -0800

    Update

--- src/app/film/page.jsx
+++ src/app/film/page.jsx
@@ -1,10 +1,35 @@
+"use client";
 import Navbars from "@/components/navbars/navbars";
+import { useEffect, useState } from "react";
 
 export default function Film() {
+    const API = process.env.NEXT_PUBLIC_OMDB_API_KEY;
+    const [movie, setMovie] = useState([]);
+
+    const response = () => {
+        fetch(API)
+            .then((res) => res.json())
+            .then(json => setMovie(json.results));
+    };
+
+    console.log(movie);
+
+    useEffect(() => {
+        response();
+    }, []);
+
     return (
         <div>
             <Navbars/>
             <h1 className="text-3xl text-black">Film</h1>
+            <div>
+                {movie.map((movie) => (
+                    <div key={movie.id}>
+                        <img src={movie.poster_path} alt={movie.title} width={200} />
+                        <p>{movie.title}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
